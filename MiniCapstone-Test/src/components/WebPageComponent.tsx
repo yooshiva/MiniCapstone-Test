9eca1447d87ba6e141b28f539b4a9126dff1da26
Pat0616 <144692539+Pat0616@users.noreply.github.com>
2025-09-21 14:18:44 +0800

    Created Whole Web Page Component (To be Fixed)

--- MiniCapstone-Test/src/components/WebPageComponent.tsx
+++ MiniCapstone-Test/src/components/WebPageComponent.tsx
@@ -1,9 +1,15 @@
+import Header from "./WebPage/Header.tsx"
+import HeaderToolBar from "./WebPage/HeaderToolBar.tsx";
+import PostPreviewGrid from "./WebPage/PostPreviewGrid.tsx";
 function WebPageComponent()
 {
 
 
     return(
         <>
+        <PostPreviewGrid></PostPreviewGrid>
+        <Header></Header>
+        <HeaderToolBar></HeaderToolBar>
         
         </>
     )
